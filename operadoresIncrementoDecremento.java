960a8783f6eb11d306345c489c645c0b0e9f27f7
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-04 09:46:07 -0300

    com7

---
+++ operadoresIncrementoDecremento.java
@@ -0,0 +1,10 @@
+public class operadoresIncrementoDecremento {
+    public static void main(String[] args) {
+        int incremento = 0;
+        System.out.println("Valor da variavel antes do incremento: " + incremento);
+        incremento++;
+        incremento++;
+        incremento++;
+        System.out.println("Valor da variavel depois do incremento: " + incremento);
+    }
+}
--- operadoresMatematicos.java
+++ operadoresMatematicos.java
@@ -4,11 +4,13 @@ public class operadoresMatematicos {
         int soma = 26 + 25;
         int multIdade = 26 * 1000;
         int divIdade = 26 / 4;
+        double divIdade2 = 26.0/4.0;
         int restoDiv = 26 % 4;
         //prints na tela
         System.out.println(soma);
         System.out.println(multIdade);
         System.out.println(divIdade);
+        System.out.println(divIdade2);
         System.out.println(restoDiv);
     }
 }
