7a5898331f03b3243f19fe4304dcfb86ddb24963
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-04 09:37:43 -0300

    com6

---
+++ operadoresMatematicos.java
@@ -0,0 +1,14 @@
+public class operadoresMatematicos {
+    public static void main(String[] args) {
+        //variáveis
+        int soma = 26 + 25;
+        int multIdade = 26 * 1000;
+        int divIdade = 26 / 4;
+        int restoDiv = 26 % 4;
+        //prints na tela
+        System.out.println(soma);
+        System.out.println(multIdade);
+        System.out.println(divIdade);
+        System.out.println(restoDiv);
+    }
+}
