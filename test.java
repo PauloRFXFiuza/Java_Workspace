438725ae63c6ce2d97294c6e06d475eb429da3d9
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-04 08:45:53 -0300

    com1

---
+++ test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main (String[] args) {
+        int a = 5, b = 3;
+        System.out.println("Hi, world!");
+        System.out.printf("%d + %d = %d",a,b,a+b);
+    }
+            
+}
