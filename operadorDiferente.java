a05a1739bc8162a9538ce6f19d5656d90192a6a9
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-04 10:04:00 -0300

    com10

---
+++ operadorDiferente.java
@@ -0,0 +1,21 @@
+public class operadorDiferente {
+    public static void main(String[] args) {
+        int num_1 = 2;
+        int num_2 = 2;
+        int num_3 = 1;
+
+        if(num_1 != num_2){
+            System.out.println("Os numeros sao diferentes.");
+        } else {
+            System.out.println("Os numeros sao iguais.");
+        }
+        System.out.println("____________________________________");
+        if(num_1 != num_3){
+            System.out.println("Os numeros sao diferentes.");
+        } else {
+            System.out.println("Os numeros sao iguais.");
+        }
+
+    }
+    
+}
