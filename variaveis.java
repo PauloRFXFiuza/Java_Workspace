76cd313abc3c95f4160cdbc5142dac675877aabd
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-04 09:05:00 -0300

    com3

--- variaveis.java
+++ variaveis.java
@@ -1,7 +1,13 @@
 public class variaveis {
     public static void main(String[] args) {
+        //Variáveis
         byte varByte = 127; //de -128 a 127
+        short varShort = 32767; //de -32768 a 32767
+        int varInt = 2147483647; //de -2147483648 a 2147483647
+        //Prints na Tela
         System.out.println(varByte);
+        System.out.println(varShort);
+        System.out.println(varInt);
     }
     
 
