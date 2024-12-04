a07f33ca27008ff4697bf4f7c8b2f62d453b510c
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-04 11:26:49 -0300

    com16

--- operadoresCondicionais.java
+++ operadoresCondicionais.java
@@ -15,43 +15,21 @@ public class operadoresCondicionais {
         }
         System.out.println("____________________________________________________________________");
         System.out.println("Mes do Ano Vigente = " + mesDoAno);
+        //o break serve para encerrar o programa caso a condição já for obedecida sem precisar rodar o resto do programa
         switch (mesDoAno){
-            case ("Janeiro"):{
-                System.out.println("Estamos no 1o Mes do Ano ");
-            }
-            case ("Fevereiro"):{
-                System.out.println("Estamos no 2o Mes do Ano ");
-            }
-            case ("Marco"):{
-                System.out.println("Estamos no 3o Mes do Ano ");
-            }
-            case ("Abril"):{
-                System.out.println("Estamos no 4o Mes do Ano ");
-            }
-            case ("Maio"):{
-                System.out.println("Estamos no 5o Mes do Ano ");
-            }
-            case ("Junho"):{
-                System.out.println("Estamos no 6o Mes do Ano ");
-            }
-            case ("Julho"):{
-                System.out.println("Estamos no 7o Mes do Ano ");
-            }
-            case ("Agosto"):{
-                System.out.println("Estamos no 8o Mes do Ano ");
-            }
-            case ("Setembro"):{
-                System.out.println("Estamos no 9o Mes do Ano ");
-            }
-            case ("Outubro"):{
-                System.out.println("Estamos no 10o e Antepenultimo Mes do Ano ");
-            }
-            case ("Novembro"):{
-                System.out.println("Estamos no 11o e Penultimo Mes do Ano ");
-            }
-            case ("Dezembro"):{
-                System.out.println("Estamos no 12o e Ultimo Mes do Ano ");
-            }
+            case ("Janeiro") -> System.out.println("Estamos no 1o Mes do Ano ");
+            case ("Fevereiro") -> System.out.println("Estamos no 2o Mes do Ano ");
+            case ("Marco") -> System.out.println("Estamos no 3o Mes do Ano ");
+            case ("Abril") -> System.out.println("Estamos no 4o Mes do Ano ");
+            case ("Maio") -> System.out.println("Estamos no 5o Mes do Ano ");
+            case ("Junho") -> System.out.println("Estamos no 6o Mes do Ano ");
+            case ("Julho") -> System.out.println("Estamos no 7o Mes do Ano ");
+            case ("Agosto") -> System.out.println("Estamos no 8o Mes do Ano ");
+            case ("Setembro") -> System.out.println("Estamos no 9o Mes do Ano ");
+            case ("Outubro") -> System.out.println("Estamos no 10o e Antepenultimo Mes do Ano ");
+            case ("Novembro") -> System.out.println("Estamos no 11o e Penultimo Mes do Ano ");
+            case ("Dezembro") -> System.out.println("Estamos no 12o e Ultimo Mes do Ano ");
+            default -> System.out.println("O Mes em Questao Esta Digitado Errado ou Nao Existe");
         }
     }
 }
