a99a612777b8dee937b348ffb2537d675a24351d
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-04 09:58:03 -0300

    com9

---
+++ operadorIgualdade.java
@@ -0,0 +1,21 @@
+public class operadorIgualdade {
+    public static void main(String[] args) {
+        char primeiraLetra = 'A';
+        char segundaLetra = 'B';
+        char terceiraLetra = 'A';
+
+        if(primeiraLetra == segundaLetra){
+            System.out.println("As letras sao iguais");
+        } else {
+            System.out.println("As letras sao diferentes");
+        }
+        System.out.println("____________________________________");
+        if(primeiraLetra == terceiraLetra){
+            System.out.println("As letras sao iguais");
+        } else {
+            System.out.println("As letras sao diferentes");
+        }
+
+    }
+    
+}
--- operadoresIncrementoDecremento.java
+++ operadoresIncrementoDecremento.java
@@ -1,10 +1,17 @@
 public class operadoresIncrementoDecremento {
     public static void main(String[] args) {
         int incremento = 0;
+        int decremento = 0;
         System.out.println("Valor da variavel antes do incremento: " + incremento);
         incremento++;
         incremento++;
         incremento++;
         System.out.println("Valor da variavel depois do incremento: " + incremento);
+        System.out.println("__________________________________________________________");
+        System.out.println("Valor da variavel antes do decremento: " + decremento);
+        decremento--;
+        decremento--;
+        decremento--;
+        System.out.println("Valor da variavel depois do decremento: " + decremento);
     }
 }
