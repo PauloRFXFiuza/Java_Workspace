2e8b59bf249c9ed5c3b9a82577668767a35c0c73
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-04 10:32:18 -0300

    com11

---
+++ operadoresRelacionais.java
@@ -0,0 +1,117 @@
+public class operadoresRelacionais {
+    public static void main(String[] args) {
+        //variaveis
+        double primeiroNumero= 13212.0;
+        double segundoNumero= 13211.1;
+        double terceiroNumero= 13211.1;
+    //1- testando o operador maior que
+        System.out.println("___________________________________________________________________________");
+        System.out.println("1- testando o operador maior que");
+        System.out.println("O primeiro numero = " + primeiroNumero);
+        System.out.println("O segundo numero = " + segundoNumero);
+        System.out.println("O terceiro numero = " + terceiroNumero);
+        System.out.println("_____");
+        if(primeiroNumero > segundoNumero){
+            System.out.println("Primeiro numero maior que o Segundo numero");
+        }else{
+            System.out.println("Primeiro numero menor ou igual que o Segundo numero");
+        }
+        System.out.println("_____");
+        if(primeiroNumero > terceiroNumero){
+            System.out.println("Primeiro numero maior que o Terceiro numero");
+        }else{
+            System.out.println("Primeiro numero menor ou igual que o Terceiro numero");
+        }
+        System.out.println("_____");
+        if(segundoNumero > terceiroNumero){
+            System.out.println("Segundo numero maior que o Terceiro numero");
+        }else{
+            System.out.println("Segundo numero menor ou igual que o Terceiro numero");
+        }
+    //2- testando o operador maior ou igual que
+        System.out.println("___________________________________________________________________________");
+        System.out.println("2- testando o operador maior ou igual que");
+        System.out.println("O primeiro numero = " + primeiroNumero);
+        System.out.println("O segundo numero = " + segundoNumero);
+        System.out.println("O terceiro numero = " + terceiroNumero);
+        System.out.println("_____");
+        if(primeiroNumero >= segundoNumero){
+            System.out.println("Primeiro numero maior ou igual que o Segundo numero");
+        }else{
+            System.out.println("Primeiro numero menor que o Segundo numero");
+        }
+        System.out.println("_____");
+        if(primeiroNumero >= terceiroNumero){
+            System.out.println("Primeiro numero maior ou igual que o Terceiro numero");
+        }else{
+            System.out.println("Primeiro numero menor que o Terceiro numero");
+        }
+        System.out.println("_____");
+        if(segundoNumero >= terceiroNumero){
+            System.out.println("Segundo numero maior ou igual que o Terceiro numero");
+        }else{
+            System.out.println("Segundo numero menor que o Terceiro numero");
+        }
+    //3- testando o operador menor que
+        System.out.println("___________________________________________________________________________");
+        System.out.println("3- testando o operador menor que");
+        System.out.println("O primeiro numero = " + primeiroNumero);
+        System.out.println("O segundo numero = " + segundoNumero);
+        System.out.println("O terceiro numero = " + terceiroNumero);
+        System.out.println("_____");
+        if(primeiroNumero < segundoNumero){
+            System.out.println("Primeiro numero menor que o Segundo numero");
+        }else{
+            System.out.println("Primeiro numero maior ou igual que o Segundo numero");
+        }
+        System.out.println("_____");
+        if(primeiroNumero < terceiroNumero){
+            System.out.println("Primeiro numero menor que o Terceiro numero");
+        }else{
+            System.out.println("Primeiro numero maior ou igual que o Terceiro numero");
+        }
+        System.out.println("_____");
+        if(segundoNumero < terceiroNumero){
+            System.out.println("Segundo numero menor que o Terceiro numero");
+        }else{
+            System.out.println("Segundo numero maior ou igual que o Terceiro numero");
+        }
+    //4- testando o operador menor ou igual que
+        System.out.println("___________________________________________________________________________");
+        System.out.println("4- testando o operador menor ou igual que");
+        System.out.println("O primeiro numero = " + primeiroNumero);
+        System.out.println("O segundo numero = " + segundoNumero);
+        System.out.println("O terceiro numero = " + terceiroNumero);
+        System.out.println("_____");
+        if(primeiroNumero <= segundoNumero){
+            System.out.println("Primeiro numero menor ou igual que o Segundo numero");
+        }else{
+            System.out.println("Primeiro numero maior que o Segundo numero");
+        }
+        System.out.println("_____");
+        if(primeiroNumero <= terceiroNumero){
+            System.out.println("Primeiro numero menor ou igual que o Terceiro numero");
+        }else{
+            System.out.println("Primeiro numero maior que o Terceiro numero");
+        }
+        System.out.println("_____");
+        if(segundoNumero <= terceiroNumero){
+            System.out.println("Segundo numero menor ou igual que o Terceiro numero");
+        }else{
+            System.out.println("Segundo numero maior que o Terceiro numero");
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+
+}
