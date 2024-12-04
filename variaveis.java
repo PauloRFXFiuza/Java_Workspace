1aefdd9a5a3f09dd4689915c7d62683284a06e8e
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-04 09:28:55 -0300

    com5

--- variaveis.java
+++ variaveis.java
@@ -2,12 +2,14 @@ public class variaveis {
     public static void main(String[] args) {
         //Variáveis
 
-        byte varByte = 127;                     //de -128 a 127
-        short varShort = 32767;                 //de -32768 a 32767
-        int varInt = 2147483647;                //de -2147483648 a 2147483647
-        long varLong = Long.MAX_VALUE;          //de −9223372036854775808 a 9223372036854775807
-        float varFloat = Float.MAX_VALUE;       //de 1.4E-45 a 3.4028235E38
-        double varDouble = Double.MAX_VALUE;    //de 4.9E-324 a 1.7976931348623157E308 
+        byte varByte = 127;                         //de -128 a 127
+        short varShort = 32767;                     //de -32768 a 32767
+        int varInt = 2147483647;                    //de -2147483648 a 2147483647
+        long varLong = 100000L;                     //de −9223372036854775808 a 9223372036854775807
+        float varFloat = 1.325E-2F;                 //de 1.4E-45 a 3.4028235E38
+        double varDouble = 1.32546545446545E-15;    //de 4.9E-324 a 1.7976931348623157E308
+        char varChar = 'a';                         //apenas um caracter entre aspas
+        boolean varBoolean = true;                  //apenas true ou false (verdadeiro ou falso)       
         
         //Prints na Tela
         
@@ -16,13 +18,19 @@ public class variaveis {
         System.out.println(varInt);
         System.out.println(varLong);
         System.out.println(varFloat);
+        System.out.println(varDouble);
+        System.out.println(varChar);
+        System.out.println(varBoolean);
         System.out.println("Valor minimo de long: " + Long.MIN_VALUE);
         System.out.println("Valor maximo de long: " + Long.MAX_VALUE);
         System.out.println("Valor minimo de float: " + Float.MIN_VALUE);
         System.out.println("Valor maximo de float: " + Float.MAX_VALUE);
         System.out.println("Valor minimo de double: " + Double.MIN_VALUE);
         System.out.println("Valor maximo de double: " + Double.MAX_VALUE);
-
+        System.out.println("Valor minimo de char: " + Character.MIN_VALUE);
+        System.out.println("Valor maximo de char: " + Character.MAX_VALUE);
+        System.out.println("Valor minimo de boolean: " + false);
+        System.out.println("Valor maximo de boolean: " + true);
     }
     
 
